efcbd4effad2ac88a25b3583dff3de7f394cd02c
pierrealexisghf <pierrealexisghf@gmail.com>
2026-07-23 10:36:17 +0200

    Create constants.js

---
+++ src/lib/constants.js
@@ -0,0 +1,5 @@
+export const PRICE_STANDARD = 'price_1Tc6S51JEMfMTdulDYObGtXg'
+export const PRICE_VIP = 'price_1Tc6Sf1JEMfMTdulDT6umOCm'
+export const CREDITS_STANDARD = 120
+
+export const SUPABASE_URL = 'https://yqvyguyxjnktajbnmavw.supabase.co'
